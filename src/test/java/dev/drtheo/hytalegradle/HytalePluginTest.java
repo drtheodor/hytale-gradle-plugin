601652f0335e63483676acbbb24fff04d3ff1923
Theo <git@theo.is-a.dev>
2026-01-15 02:59:40 +0300

    fix

--- src/test/java/dev/drtheo/hytalegradle/HytalePluginTest.java
+++ src/test/java/dev/drtheo/hytalegradle/HytalePluginTest.java
@@ -5,6 +5,10 @@ import org.gradle.testkit.runner.BuildResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HytalePluginTest {
     
@@ -12,7 +16,7 @@ class HytalePluginTest {
     File testProjectDir;
     
     @Test
-    void pluginRegistersTasks() {
+    void pluginRegistersTasks() throws IOException {
         // Write build.gradle
         File buildFile = new File(testProjectDir, "build.gradle");
         String buildFileContent = """
